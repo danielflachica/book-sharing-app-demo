2bee86911afad216dec98cdf028c0aaa7b93b323
Daniel Lachica <daniel.lachica82@gmail.com>
2022-02-23 00:05:24 +0800

    Add delete book function

--- resources/js/dependencies/vue-components/BookPreview.vue
+++ resources/js/dependencies/vue-components/BookPreview.vue
@@ -1,6 +1,27 @@
 <template>
-  <div class="flex flex-col bg-white shadow border">
-    <img v-if="cover_photo" class="h-96 w-auto object-cover bg-gray-200" :src="cover_photo_path" :alt="title">
+  <div class="flex flex-col bg-white shadow border relative">
+    <options-dropdown 
+      v-if="editable != null" 
+      class="absolute top-2 right-2">
+      <confirm-button class="options-dropdown-item-danger"
+        :title="'Are you sure you want to delete '+title+'?'"
+        submit-button-text="Delete Book"
+        :href="delete_route"
+        :csrf="csrf"
+        method="DELETE">
+        Delete Book
+      </confirm-button>
+    </options-dropdown>
+
+    <img v-if="cover_photo" 
+      class="h-96 w-auto object-cover bg-gray-200" 
+      :src="cover_photo_path" 
+      :alt="title">
+    <img v-else 
+      class="h-96 w-auto object-cover bg-gray-200"
+      src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/2048px-No_image_available.svg.png"
+      alt="No Image Available">
+    
     <div class="p-4">
       <div v-if="title">
         Title: <span :class="textColor">{{ title }}</span>
@@ -24,7 +45,17 @@
 <script>
   export default {
     name: "book-preview",
-    props: ["cover_photo", "title", "isbn", "year", "added_by", "last_updated"],
+    props: [
+      "cover_photo",
+      "title",
+      "isbn",
+      "year",
+      "added_by",
+      "last_updated",
+      "editable",
+      "csrf",
+      "delete_route",
+    ],
     data() {
       return {
         textColor: 'text-primary',
