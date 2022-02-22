ac7af24ba842cade2e1f403feb3f3394ddcede08
Daniel Lachica <daniel.lachica82@gmail.com>
2022-02-22 14:25:35 +0800

    Add book catalogue to home page

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -2,34 +2,35 @@
 
 @section('content')
   <div class="container mx-auto p-4 md:p-8">
-    <div class="flex flex-col gap-4 md:gap-8">
+    <div class="flex flex-col gap-4 md:gap-8 pt-12">
 
-      <div class="grid grid-cols-1">
-        <card-basic title="Card Title">
-          <div class="flex flex-col items-center justify-center p-8 gap-4">
-            <span class="text-center">
-              Hello, {{ Auth::user()->name }}!
-              {{ __('You are logged in!') }}
-            </span>
+      @component('components.breadcrumb')
+        @slot('current_page', 'Home')
+      @endcomponent
 
+      <card-basic title="Book Catalogue">
+        <div class="grid grid-col-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">
+
+          @foreach($books as $book)
+            <book-preview
+              cover_photo="{{ $book->coverPhoto ? Storage::disk('public')->url($book->coverPhoto->image_path) : 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/2048px-No_image_available.svg.png' }}"
+              title="{{ $book->title }}"
+              isbn="{{ $book->isbn }}"
+              year="{{ $book->year }}"
+              added_by="{{ $book->uploader->name }}"
+              last_updated="{{ $book->updated_at->diffForHumans() }}" />
+            </book-preview>
+          @endforeach
+
+        </div>
+        <template v-slot:footer>
+          <div class="flex flex-wrap justify-end">
             <a href="{{ route('user.books.create') }}" class="btn-outlined-primary">
               Add a Book
             </a>
-
-            @if(Route::has('logout'))
-              <form method="POST" action="{{ route('logout') }}">
-                @csrf
-                <a class="btn-primary"
-                  href="{{ route('logout') }}"
-                  onclick="event.preventDefault(); this.closest('form').submit();">
-                  <i class="material-icons mr-2">exit_to_app</i>
-                  Logout
-                </a>
-              </form>
-            @endif
           </div>
-        </card-basic>
-      </div>
+        </template>
+      </card-basic>
 
     </div>
   </div>
